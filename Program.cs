8191a828718bf1ad166dcf85bcb8fb93a97d3815
Joel Fernandes <joel.fernandes@kilowott.com>
2021-11-30 13:19:13 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@ namespace ConsoleApp2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the first line.");
+
+            Console.Write("This is ");
+            Console.Write("the second ");
+            Console.Write("line.");
+            challenge.ShowOutput();
         }
     }
 }
---
+++ challenge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class challenge
+    {
+        public static void ShowOutput()
+        {
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+            Console.WriteLine("The temperature is " + celsius + " Celsius.");
+        }
+    }
+}
